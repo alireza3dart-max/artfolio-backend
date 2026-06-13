6c6554ef488843b9479d71f1b29c7bb6a12d3b6a
alireza3dart-max <alireza.3dart@gmail.com>
2026-06-13 23:05:34 +0330

    fix user model followers following

--- models/User.js
+++ models/User.js
@@ -1,20 +1,82 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  avatar: { type: String, default: 'https://i.pravatar.cc/150?img=12' },
-  bio: { type: String, default: '' },
-  location: { type: String, default: '' },
-  website: { type: String, default: '' },
-  twitter: { type: String, default: '' },
-  instagram: { type: String, default: '' },
-  role: { type: String, default: '3D Artist' },
-  followers: { type: Number, default: 0 },
-  following: { type: Number, default: 0 },
-  followedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  followingList: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 2,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  googleId: {
+    type: String,
+    default: null,
+  },
+  avatar: {
+    type: String,
+    default: '',
+  },
+  bio: {
+    type: String,
+    default: '',
+    maxlength: 500,
+  },
+  location: {
+    type: String,
+    default: '',
+  },
+  website: {
+    type: String,
+    default: '',
+  },
+  twitter: {
+    type: String,
+    default: '',
+  },
+  instagram: {
+    type: String,
+    default: '',
+  },
+  role: {
+    type: String,
+    default: '3D Artist',
+  },
+  followers: {
+    type: Number,
+    default: 0,
+  },
+  following: {
+    type: Number,
+    default: 0,
+  },
+  followingList: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  }],
+  followedBy: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  }],
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
